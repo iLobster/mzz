b49ce63301329b67377dbadd898a9b20fa344760
ml <no@mail>
2005-11-12 15:19:17 +0000

    Документирование кода

--- system/config/config.php
+++ system/config/config.php
@@ -1,15 +1,58 @@
 <?php
-// класс работы с конфигурацией
-
+//
+// $Id$
+// $URL$
+//
+// MZZ Content Management System (c) 2005
+// Website : http://www.mzz.ru
+//
+// This program is free software and released under
+// the GNU/GPL License (See /docs/GPL.txt).
+//
+/**
+ * DB - класс для работы с конфигурацией
+ *
+ * @package system
+ * @version 0.1
+*/
 class config
 {
+    /**
+     * Свойство для хранения результата обработки конфиг-файла
+     *
+     * @var array
+     * @access protected
+     */
     protected $_ini;
+
+    /**
+     * Имя обработанного конфиг-файла
+     *
+     * @var string
+     * @access protected
+     */
     protected $_ini_file;
 
+    /**
+     * Constructor
+     *
+     * @access public
+     * @return void
+     */
     public function __construct() {
 
     }
 
+    /**
+     * Загрузка и обработка конфиг-файла. Результат обработки сохраняется и при
+     * повторном вызове метода load с тем же именем конфиг-файла будет возвращен
+     * сохраненный результат. Для обновления результата используется метод update.
+     *
+     * @access public
+     * @param string $file имя файла (без '.ini' в конце)
+     * @param boolean $process_sections
+     * @return bolean
+     */
     public function load($file, $process_sections = true)
     {
         $file = CONFIG_DIR . $file . '.ini';
@@ -26,6 +69,14 @@ class config
 
     }
 
+    /**
+     * Получение значения опции
+     *
+     * @access public
+     * @param string $section имя секции
+     * @param string $name имя опции
+     * @return string|false
+     */
     public function getOption($section, $name)
     {
         if(isset($this->_ini[$section][$name])) {
@@ -35,6 +86,14 @@ class config
         }
 
     }
+
+    /**
+     * Получение всей секции
+     *
+     * @access public
+     * @param string $section имя секции
+     * @return array|false
+     */
     public function getSection($section)
     {
         if(isset($this->_ini[$section])) {
@@ -43,6 +102,13 @@ class config
             return false;
         }
     }
+
+    /**
+     * Обновление результата обработки
+     *
+     * @access public
+     * @return void
+     */
     public function update() {
         $file = $this->_ini_file;
         unset($this->_ini_file);
@@ -50,7 +116,5 @@ class config
     }
 
 }
-
-
 
 ?>
--- system/config/configFactory.php
+++ system/config/configFactory.php
@@ -1,16 +1,40 @@
 <?php
-// фабрика конфигуратора
-
-fileResolver::includer('config');
-
+//
+// $Id$
+// $URL$
+//
+// MZZ Content Management System (c) 2005
+// Website : http://www.mzz.ru
+//
+// This program is free software and released under
+// the GNU/GPL License (See /docs/GPL.txt).
+//
+/**
+ * Фабрика конфигурации
+ *
+ * @package system
+ * @version 0.1
+ */
 class configFactory
 {
-    public static $instance;
+    /**
+     * Singleton
+     *
+     * @staticvar
+     * @var object
+     * @access private
+     */
+    private static $instance;
 
+    /**
+     *
+     * @access public
+     * @return object
+     */
 	public function getInstance()
 	{
-
 		if ( !isset( $instance ) ) {
+		    fileResolver::includer('config');
 			$instance = new config();
 		}
 		return $instance;
