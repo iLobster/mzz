be659213852fe961cff52d35c81f1739aca44295
ml <no@mail>
2005-11-12 00:29:38 +0000

    Added generation time

--- www/index.php
+++ www/index.php
@@ -1,17 +1,8 @@
 <?php
+define('_START_TIME', microtime(true));
+
 require_once './config.php';
 require_once SYSTEM . 'index.php';
-//require_once SYSTEM . '/request/httprequest.php';
 
-
-/*
-$f = new File('test.txt','r', APPLICATION );
-$f->write("5353535");
-$f->seek(0);
-echo $f->read(120);*/
-
-/*
-$_GET['z'] = 'bla';
-echo '<br>';
-var_dump(HttpRequest::get('z'));*/
+echo "<br><hr size=1><font size=-2>Time: ".(microtime(true)-_START_TIME);
 ?>
