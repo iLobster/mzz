50b9fb8538e6ed7f62f2e3aa0da9aa0feb3d5cb1
zerkms <zerkms@zerkms.ru>
2005-12-12 13:27:10 +0000

    ""

--- www/tests/cases/modules/news/newsActiveRecord.case.php
+++ www/tests/cases/modules/news/newsActiveRecord.case.php
@@ -1,16 +1,20 @@
 <?php
 
 fileLoader::load('news/newsActiveRecord');
+fileLoader::load('news/newsTableModule');
+
+mock::generate('newsTableModule');
 
 class newsActiveRecordTest extends unitTestCase
 {
     protected $newsAR;
     public function setUp()
     {
-        $this->newsAR = new newsActiveRecord();
+        $TM = new mocknewsTableModule();
+        $this->newsAR = new newsActiveRecord($stmt, $TM);
     }
     
-    public function testX()
+    public function testFirst()
     {
         
     }
