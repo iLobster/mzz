d9ac0038a8a299a189081d6ee28e147c3ac6a475
meze <mz.evgeny@gmail.com>
2006-03-22 21:01:16 +0000

    refactoring :)

--- system/template/plugins/function.add.php
+++ system/template/plugins/function.add.php
@@ -20,7 +20,7 @@
  *
  * @param array $params входные аргументы функции
  * @param object $smarty объект смарти
- * @return string результат работы модуля
+ * @return boolean null если файл дубликат
  * @package system
  * @version 0.1
  */
@@ -28,56 +28,53 @@ function smarty_function_add($params, $smarty)
 {
     $valid_resources = array('css', 'js');
 
-    if (!empty($params['file'])) {
-        // определяем тип ресурса
-        $tmp = array_map('trim', explode(':', $params['file'], 2));
-
-        if (sizeof($tmp) == 2 && strlen($tmp[0]) > 0) {
-            $res = $tmp[0];
-            $filename = $tmp[1];
-        } else {
-            $params['file'] = str_replace(':', '', $params['file']);
-            $res = substr(strrchr($params['file'], '.'), 1);
-            $filename = $params['file'];
-        }
-
-        if (!in_array($res, $valid_resources)) {
-            throw new mzzInvalidParameterException('Неверный тип ресурса', $res);
-        }
-
-        if (!preg_match('/^[a-z0-9_\.?&=]+$/i', $filename)) {
-            throw new mzzInvalidParameterException('Неверное имя файла', $filename);
-        }
-        // проверить имя файла?
-
-        $tpl = (!empty($params['tpl'])) ? $params['tpl'] : $res . '.tpl';
-        // проверить имя файла шаблона
-
-
-        $vars = $smarty->get_template_vars($res);
-        // если массив ещё пустой - создаём
-        if ($vars === null) {
-            $smarty->assign($res, array());
-        }
-
-        $added = false;
-
-        // ищем - подключали лы мы уже данный файл
-        if (is_array($vars)) {
-            foreach ($vars as $val) {
-                if ($val['file'] == $filename && $val['tpl'] == $tpl) {
-                    $added = true;
-                    break;
-                }
-            }
-        }
-
-        if (!$added) {
-            $smarty->append($res, array('file' => $filename, 'tpl' => $tpl));
-        }
-    } else {
+    if (empty($params['file'])) {
         throw new mzzInvalidParameterException('Пустой аттрибут', 'file');
     }
+
+    // определяем тип ресурса
+    // Первая часть - тип ресурс, вторая - файл ресурса
+    $tmp = array_map('trim', explode(':', $params['file'], 2));
+
+    if (sizeof($tmp) == 2 && strlen($tmp[0]) > 0) {
+        // Ресурс указан
+        $res = $tmp[0];
+        $filename = $tmp[1];
+    } else {
+        // Ресурс не указан, пытаемся определить ресурс по расширению
+        $params['file'] = str_replace(':', '', $params['file']);
+        $res = substr(strrchr($params['file'], '.'), 1);
+        $filename = $params['file'];
+    }
+
+    if (!in_array($res, $valid_resources)) {
+        throw new mzzInvalidParameterException('Неверный тип ресурса', $res);
+    }
+
+    if (!preg_match('/^[a-z0-9_\.?&=]+$/i', $filename)) {
+        throw new mzzInvalidParameterException('Неверное имя файла', $filename);
+    }
+
+    // Если шаблон не указан, то используем шаблон соответствующий расширению
+    $tpl = (!empty($params['tpl'])) ? $params['tpl'] : $res . '.tpl';
+
+    $vars = $smarty->get_template_vars($res);
+    // если массив ещё пустой - создаём
+    if ($vars === null) {
+        $smarty->assign($res, array());
+    }
+
+    // ищем - подключали ли мы уже данный файл
+    if (is_array($vars)) {
+        foreach ($vars as $val) {
+            if($val['file'] == $filename && $val['tpl'] == $tpl) {
+                return null;
+            }
+        }
+    }
+
+    $smarty->append($res, array('file' => $filename, 'tpl' => $tpl));
+
 }
 
 ?>
