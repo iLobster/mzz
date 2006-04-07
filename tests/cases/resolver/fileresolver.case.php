bb56c0780bba3031c8eda26fccb4a1f934bbb886
meze <mz.evgeny@gmail.com>
2006-04-07 15:49:45 +0000

    bug fixed

--- tests/cases/resolver/fileresolver.case.php
+++ tests/cases/resolver/fileresolver.case.php
@@ -7,14 +7,14 @@ class fileResolverTest extends unitTestCase
     public $resolver;
     function setUp()
     {
-        $this->resolver = new fileResolver(systemConfig::$pathToSystem . '../tests/cases/resolver/*');
+        $this->resolver = new fileResolver(realpath(dirname(__FILE__)) . '/*');
     }
-    
+
     function testResolveExist()
     {
         $this->assertEqual(realpath(dirname(__FILE__) . '/' . basename(__FILE__)), realpath($this->resolver->resolve(basename(__FILE__))));
     }
-    
+
     function testResolveNotExist()
     {
         $this->assertNull($this->resolver->resolve(''));
