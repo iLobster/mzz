782bf4f8e5cdf2e214d3fc0941319312dbd9d269
meze <mz.evgeny@gmail.com>
2006-10-04 20:28:35 +0000

    fix routes rules

--- www/configs/routes.php
+++ www/configs/routes.php
@@ -1,9 +1,12 @@
 <?php
 
+
 $router->addRoute('default', new requestRoute('', array('section' => 'news', 'action' => 'list')));
-$router->addRoute('withName', new requestRoute(':section/:name/:action', array('action' => 'view')));
-$router->addRoute('withoutParams', new requestRoute(':section/:action', array('action' => 'list')));
+$router->addRoute('newsCategory', new requestRoute('news/:name/:action', array('section' => 'news', 'action' => 'list')));
+$router->addRoute('pageNamed', new requestRoute('page/:name/:action', array('section' => 'page', 'action' => 'view')));
+$router->addRoute('pageActions', new requestRoute('page/:action', array('section' => 'page', 'action' => 'list'), array('action' => '(?:list|create|delete)')));
 $router->addRoute('withId', new requestRoute(':section/:id/:action', array('action' => 'view'), array('id' => '\d+')));
-$router->addRoute('pageName', new requestRoute('page/:name/:action', array('section' => 'page', 'action' => 'view')));
+$router->addRoute('newsActions', new requestRoute('news/:action', array('section' => 'news', 'action' => 'list'), array('action' => '(?:list|createItem|delete)')));
+
 
 ?>
