db4849077fcc9c49585ef0da424a99fe7621883e
zerkms <zerkms@zerkms.ru>
2007-02-06 22:00:17 +0000

    фикс роутов

--- www/configs/routes.php
+++ www/configs/routes.php
@@ -8,7 +8,7 @@ $router->addRoute('cfgEdit', new requestRoute('config/:section_name/:module_name
 $router->addRoute('fmFolder', new requestRoute('fileManager/:name/:action', array('section' => 'fileManager', 'name' => 'root', 'action' => 'list'), array('name' => '.+?', 'action' => '(?:list|upload)')));
 $router->addRoute('fmFolderRoot', new requestRoute('fileManager/:action', array('section' => 'fileManager', 'name' => 'root', 'action' => 'list'), array('name' => '.+?', 'action' => '(?:list|upload)')));
 
-$router->addRoute('newsFolder', new requestRoute('news/:name/:action', array('section' => 'news', 'name' => 'root', 'action' => 'list'), array('action' => '(?:list|create|createFolder|editFolder|deleteFolder)')));
+$router->addRoute('newsFolder', new requestRoute('news/:name/:action', array('section' => 'news', 'name' => 'root', 'action' => 'list'), array('name' => '.+?', 'action' => '(?:list|create|createFolder|editFolder|deleteFolder)')));
 $router->addRoute('pageActions', new requestRoute('page/:name/:action', array('section' => 'page', 'name' => 'root', 'action' => 'view'), array('name' => '.+?', 'action' => '(?:view|edit|list|create|delete|createFolder)')));
 $router->addRoute('pageDefault', new requestRoute('page', array('section' => 'page', 'action' => 'view', 'name' => 'main')));
 
