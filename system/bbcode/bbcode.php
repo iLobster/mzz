0df99a09344821123c3d5db7ed900e9617a3fdba
wiistriker <wiistriker@gmail.com>
2008-01-15 09:38:31 +0000

    bbcode + tests

--- system/bbcode/bbcode.php
+++ system/bbcode/bbcode.php
@@ -4,9 +4,25 @@ class bbcode
     protected $content;
 
     protected $bbcodes = array(
-        'img' => 'img',
-        'b' => 'strong',
-        'i' => 'i',
+        'b' => array(
+                    'tag' => 'strong',
+                    'attributes' => array()
+                ),
+        'i' => array(
+                    'tag' => 'em',
+                    'attributes' => array()
+                ),
+        'font' => array(
+                    'tag' => 'font',
+                    'attributes' => array(
+                                    'size' => array(
+                                                '+1', '+2', '+3', '+4'
+                                              ),
+                                    'font-family' => array(
+                                              'Verdana', 'Tahoma'
+                                              )
+                                )
+                ),
     );
 
     public function __construct($content = null)
@@ -18,18 +34,57 @@ class bbcode
 
     public function parse()
     {
-        $content = $this->content;
-        $content = preg_replace_callback('!\[(\/?)(' . join('|', array_keys($this->bbcodes)) .')([^\]]*)\]!Uu', array('self', 'parseTag'), $content);
-        echo htmlspecialchars($content) . '<br />';
+        $content = htmlspecialchars($this->content);
+        //$content = preg_replace_callback('!\[(\/?)(' . join('|', array_keys($this->bbcodes)) .')([^\]]*)\]!Uu', array('self', 'parseTag'), $content);
+
+        $content = preg_replace_callback('!\[(' . join('|', array_keys($this->bbcodes)) .')([^\]]*)\](.*)\[/\\1\]!Uiu', array('self', 'parseTag'), $content);
+        //echo htmlspecialchars($content) . '<br />';
+
         return $content;
     }
 
     public function parseTag($matches)
     {
-        list($match, $add, $tag, $attributes) = $matches;
-        $attributes = trim($attributes);
+        list($original, $tag, $attributes, $content) = $matches;
 
-        return '<' . $add . $this->bbcodes[$tag] . (($add != '/' && !empty($attributes)) ? ' ' .$attributes : null) .'>';
+        $validatedAttributes = null;
+
+        if ($attributes) {
+            if (strpos($attributes, ' ') !== 0) {
+                return htmlspecialchars_decode($original);
+            }
+
+            $tmpAtr = array();
+            foreach ($this->parseAttributes($tag, $attributes) as $key => $value) {
+                //если такой аттрибут уже передавался, либо если такой аттрибут запрещен в этом теге, либо если такое значение аттрибута запрещено,
+                //то выводить оригинал
+                if (isset($tmpAtr[$key]) || !isset($this->bbcodes[$tag]['attributes'][$key]) || !in_array($value, $this->bbcodes[$tag]['attributes'][$key])) {
+                    return htmlspecialchars_decode($original);
+                }
+
+                $tmpAtr[$key] = $value;
+                $validatedAttributes .= $key . '="' . $value . '" ';
+            }
+            $validatedAttributes = trim($validatedAttributes);
+        }
+
+        return htmlspecialchars_decode('<' . $this->bbcodes[$tag]['tag'] . ($validatedAttributes ? ' ' . $validatedAttributes : null) .'>' . $content . '</' . $this->bbcodes[$tag]['tag'] . '>');
+    }
+
+    protected function parseAttributes($tag, $attributes)
+    {
+        if (preg_match_all('!(.*)="(.*)"!U', htmlspecialchars_decode($attributes), $matches)) {
+            $atrs = array();
+            for ($i = 0; $i < count($matches[0]); $i++) {
+                $key = trim($matches[1][$i]);
+                $value = trim($matches[2][$i]);
+                $atrs[$key] = $value;
+            }
+
+            return $atrs;
+        }
+
+        return array();
     }
 }
 ?>
--- tests/cases/bbcode/bbcode.case.php
+++ tests/cases/bbcode/bbcode.case.php
@@ -18,12 +18,45 @@ class bbcodeTest extends unitTestCase
     {
         $bbcode_parser = new bbcode('[b]test[/b]');
         $this->assertEqual('<strong>test</strong>', $bbcode_parser->parse());
+
+        $bbcode_parser = new bbcode('[batr="test"]test[/b]');
+        $this->assertEqual('[batr="test"]test[/b]', $bbcode_parser->parse());
+
+        $bbcode_parser = new bbcode('[i]test[/i]');
+        $this->assertEqual('<em>test</em>', $bbcode_parser->parse());
     }
 
     public function testParseBBCodeWithAttributes()
     {
-        $bbcode_parser = new bbcode('[img src="http://ya.ru/logo.gif"]test[/img]');
-        $this->assertEqual('<img src="http://ya.ru/logo.gif">test</img>', $bbcode_parser->parse());
+        $bbcode_parser = new bbcode('[font size="+1"]test[/font]');
+        $this->assertEqual('<font size="+1">test</font>', $bbcode_parser->parse());
+
+        $bbcode_parser = new bbcode('[font size="+1" font-family="Verdana"]test[/font]');
+        $this->assertEqual('<font size="+1" font-family="Verdana">test</font>', $bbcode_parser->parse());
+
+        $bbcode_parser = new bbcode('[font font-family="Verdana" size="+1"]test[/font]');
+        $this->assertEqual('<font font-family="Verdana" size="+1">test</font>', $bbcode_parser->parse());
+    }
+
+    public function testParseBBCodeWithWrongAttributes()
+    {
+        $bbcode_parser = new bbcode('[font not_exist="+1"]test[/font]');
+        $this->assertEqual('[font not_exist="+1"]test[/font]', $bbcode_parser->parse());
+    }
+
+    public function testParseBBCodeWithWrongAttributesValues()
+    {
+        $bbcode_parser = new bbcode('[font size="+999"]test[/font]');
+        $this->assertEqual('[font size="+999"]test[/font]', $bbcode_parser->parse());
+    }
+
+    public function testParseBBCodeUnclosed()
+    {
+        $bbcode_parser = new bbcode('[b]test[/b');
+        $this->assertEqual('[b]test[/b', $bbcode_parser->parse());
+
+        $bbcode_parser = new bbcode('test[/b]');
+        $this->assertEqual('test[/b]', $bbcode_parser->parse());
     }
 }
 ?>
