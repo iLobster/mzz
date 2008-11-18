d47a42f883a20a3b057e783e535d14789cc880e3
zerkms <zerkms@zerkms.ru>
2008-11-18 09:29:07 +0000

    Удаление ratings.js

--- www/templates/js/ratings.js
+++
@@ -1,9 +0,0 @@
-function rate(url, container)
-{
-    new Ajax.Request(url, {
-        method: 'get',
-        onSuccess: function(transport) {
-            container.update(transport.responseText);
-        }
-    });
-}
