01bb014350082d6205c37db87a9e180fd431bf6d
zerkms <zerkms@zerkms.ru>
2008-12-11 00:24:24 +0000

    забыл поставить кейворды

--- system/resolver/extensionBasedModuleMediaResolver.php
+++ system/resolver/extensionBasedModuleMediaResolver.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $URL: svn://svn.subversion.ru/usr/local/svn/mzz/trunk/system/resolver/moduleMediaResolver.php $
+ * $URL$
  *
  * MZZ Content Management System (c) 2005-2007
  * Website : http://www.mzz.ru
@@ -9,7 +9,7 @@
  * the GNU/GPL License (See /docs/GPL.txt).
  *
  * @link http://www.mzz.ru
- * @version $Id: moduleMediaResolver.php 2859 2008-12-10 23:51:32Z zerkms $
+ * @version $Id$
  */
 
 require_once systemConfig::$pathToSystem . '/resolver/partialFileResolver.php';
