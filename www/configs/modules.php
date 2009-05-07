1cd9096117e2feed5c893221884e5e00d99d6854
zerkms <zerkms@zerkms.ru>
2009-05-07 01:00:27 +0000

    remove unnecessary module-section assotiations from config

--- www/configs/modules.php
+++ www/configs/modules.php
@@ -3,23 +3,13 @@
 $modules = array (
     'access' => 'access',
     'admin' => 'admin',
-    'browser' => 'browser',
-    'captcha' => 'captcha',
-    'catalogue' => 'catalogue',
     'comments' => 'comments',
     'config' => 'config',
-    'faq' => 'faq',
     'fileManager' => 'fileManager',
-    'forum' => 'forum',
-    'gallery' => 'gallery',
     'menu' => 'menu',
-    'message' => 'message',
     'news' => 'news',
     'page' => 'page',
-    'ratings' => 'ratings',
-    'tags' => 'tags',
     'user' => 'user',
-    'voting' => 'voting',
 );
 
 
