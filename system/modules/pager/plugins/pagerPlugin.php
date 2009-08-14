276ae161b7641af56176f87058a8eda53ba669cb
wiistriker <wiistriker@gmail.com>
2009-08-14 01:14:12 +0000

    revert of pagerPlugin criteriaForCount

--- system/modules/pager/plugins/pagerPlugin.php
+++ system/modules/pager/plugins/pagerPlugin.php
@@ -13,8 +13,7 @@ class pagerPlugin extends observer
     public function preSqlSelect(criteria $criteria)
     {
         $criteriaForCount = clone $criteria;
-        //$criteriaForCount->clearSelectFields()->addSelectField(new sqlFunction('count', new sqlOperator('DISTINCT', $this->mapper->table(false) . '.' . $this->mapper->pk())), 'cnt');
-        $criteriaForCount->clearSelectFields()->addSelectField(new sqlFunction('count', '*', true), 'cnt');
+        $criteriaForCount->clearSelectFields()->addSelectField(new sqlFunction('count', new sqlOperator('DISTINCT', $this->mapper->table(false) . '.' . $this->mapper->pk())), 'cnt');
         $selectForCount = new simpleSelect($criteriaForCount);
 
         $this->pager->setCount($this->mapper->db()->getOne($selectForCount->toString()));
