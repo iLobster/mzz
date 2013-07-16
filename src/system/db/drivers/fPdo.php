1b5d694d4099f3d79dea26bdc07a5c7a30b1e581
bobrosoft <bobrosoft@yandex.ru>
2013-07-16 21:47:52 +0100

    fPdo: added transactions and nested transactions support

--- src/system/db/drivers/fPdo.php
+++ src/system/db/drivers/fPdo.php
@@ -52,6 +52,12 @@ class fPdo extends PDO
      * @var string
      */
     private $tablePrefix;
+    
+    // Database drivers that support SAVEPOINTs.
+    protected static $savepointTransactions = array("pgsql", "mysql");
+
+    // The current transaction level.
+    protected $transLevel = 0;
 
     /**
      * Декорируем конструктор PDO
@@ -237,7 +243,50 @@ class fPdo extends PDO
     {
         return $this->tablePrefix;
     }
+    
+    protected function nestable()
+    {
+        return in_array($this->getAttribute(PDO::ATTR_DRIVER_NAME),
+                        self::$savepointTransactions);
+    }
+
+    public function beginTransaction()
+    {
+        if(!$this->nestable() || $this->transLevel == 0) {
+            parent::beginTransaction();
+        } else {
+            $this->exec("SAVEPOINT LEVEL{$this->transLevel}");
+        }
+
+        $this->transLevel++;
+        return true;
+    }
+
+    public function commit()
+    {
+        $this->transLevel--;
+
+        if(!$this->nestable() || $this->transLevel == 0) {
+            parent::commit();
+        } else {
+            $this->exec("RELEASE SAVEPOINT LEVEL{$this->transLevel}");
+        }
+
+        return true;
+    }
+
+    public function rollBack()
+    {
+        $this->transLevel--;
+
+        if(!$this->nestable() || $this->transLevel == 0) {
+            parent::rollBack();
+        } else {
+            $this->exec("ROLLBACK TO SAVEPOINT LEVEL{$this->transLevel}");
+        }
+
+        return true;
+    }
 
 }
-
 ?>
